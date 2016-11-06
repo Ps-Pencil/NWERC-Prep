6ce5120eefdd6d07cdaf90c8ca87e2cd9ceb7e46
Song Pan <pan.song@dhs.sg>
2016-11-06 17:37:47 +0000

    added some

---
+++ 10071.cpp
@@ -0,0 +1,13 @@
+// https://uva.onlinejudge.org/external/100/10071.pdf
+
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+  int v,t;
+  while(scanf("%d %d",&v,&t)==2){
+    printf("%d\n",t*(v)*2);
+  }
+  
+}
