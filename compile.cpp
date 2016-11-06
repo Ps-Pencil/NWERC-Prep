9ab6c4e3f0a5fa2decd687ef798cc29d07b289d6
raoxj <raoxiaojia@gmail.com>
2016-11-06 17:35:46 +0000

    compile

--- compile.cpp
+++ compile.cpp
@@ -38,21 +38,30 @@ int main()
 				if (i==0) d[1][j][k] = min(d[1][j][k],d[i][j][k]+1);//set i to 1
 				if (j==0) d[i][1][k] = min(d[i][1][k],d[i][j][k]+1);//set j to 1
 				if (k==0) d[i][j][1] = min(d[i][j][1],d[i][j][k]+1);//set k to 1
+				
 				d[j][j][k] = min(d[j][j][k],d[i][j][k]+1);//set i to j
 				d[k][j][k] = min(d[k][j][k],d[i][j][k]+1);//set i to k
 				for (l=2;l<=5;l++)//multiplication
 				{
-					if (i*l <= 255)//pop z;push z (l-1 times);add (l-1 times);
+					if (i*l <= 255)//pop z;push z (l times);add (l-1 times);
 					{
-						d[i*l][j][k] = min(d[i*l][j][k],d[i][j][k]+2*l-1);
+						d[i*l][j][k] = min(d[i*l][j][k],d[i][j][k]+2*l);
 					}
 					if (j*l <= 255)//push x (l times);add (l-1 times); pop x;
 					{
 						d[i][j*l][k] = min(d[i][j*l][k],d[i][j][k]+2*l);
 					}
+					if (i+j*l <= 255)//push x (l times);add (l times);
+					{
+						d[i+j*l][j][k] = min(d[i+j*l][j][k],d[i][j][k]+2*l);
+					}
 					if (k*l <= 255)//same
 					{
 						d[i][j][k*l] = min(d[i][j][k*l],d[i][j][k*l]+2*l);
+					}
+					if (i+k*l <= 255)//same
+					{
+						d[i+k*l][j][k] = min(d[i+k*l][j][k],d[i][j][k]+2*l);
 					}
 				}
 				if (j+k<=255)//push x;push y;add; pop x/y;
@@ -60,13 +69,13 @@ int main()
 					d[i][j+k][k] = min(d[i][j+k][k],d[i][j][k]+4);
 					d[i][j][j+k] = min(d[i][j][j+k],d[i][j][k]+4);
 				}
-				if (i+j<=255)//push x;add;pop x;push x;
+				if (i+j<=255)
 				{
-					d[i+j][i+j][k] = min(d[i+j][i+j][k],d[i][j][k]+4);
+					d[i+j][i+j][k] = min(d[i+j][i+j][k],d[i][j][k]+4);//push x;add;pop x;push x;
 				}
-				if (i+k<=255)//same
+				if (i+k<=255)
 				{
-					d[i+k][j][i+k] = min(d[i+k][j][i+k],d[i][j][k]+4);
+					d[i+k][j][i+k] = min(d[i+k][j][i+k],d[i][j][k]+4);//same
 				}
 				
 				ans[i] = min(ans[i],d[i][j][k]+2);//pop; dl
